943970c0aadbf6ae67a990fe22d1a4d6d7da3056
UmarovSino <abubakrs058@gmail.com>
2025-04-21 01:37:33 +0500

    to shop our best..

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import "./App.css"
 import imgMagazin from "./assets/Vector (11).png"
 import asaiding from "./assets/IMAGE (44).png"
+import imgsection from "./assets/IMAGE (45).png"
+import img2 from "./assets/IMAGE (46).png"
+import img3 from "./assets/IMAGE (47).png"
 import Button from './component/button'
 const App = () => {
   return (
@@ -44,7 +47,56 @@ const App = () => {
           </h2>
           <p className='keping-p'>This isn’t just another rental company - We’re elevating the future of furniture by replacing the commitment of ownership with a culture of sharing in a way that’s curated, convenient, and sustainable.</p>
 
-      
+
+        </section>
+        <section className='curated-section'>
+          <aside>
+            <div className='convenien-div'>
+              <h2 className='cura-teds'>
+                Enjoy a fully furnished space in less than week
+              </h2>
+              <p className='buil-divhs'>Feather furniture is available to be delivered and assembled in New York City, Los Angeles and San Francisco.</p>
+              <p className='explore-furniture'>Explore Furniture </p>
+            </div>
+          </aside>
+          <aside>
+            <img src={asaiding} className='asaidinimge' alt="" />
+          </aside>
+        </section>
+        <section className='garge-section'>
+          <aside>
+            <img src={imgsection} alt="" />
+          </aside>
+          <aside>
+            <h2 className='garage-sale'>Garage Sale - 40% & Up</h2>
+            <p className='garage-saleopen'>Our Garage Sale is open! Savings start at 40% on favorite retired pieces. Find dressers, sofas, decor, and more to make the year one to remember.</p>
+            <p className='terms-sale'>Terms and conditions apply.View here.</p>
+            <p className='shop-now'>Shop Now</p>
+          </aside>
+        </section>
+        <section className='curated-section'>
+          <aside>
+            <div className='convenien-div'>
+              <h2 className='cura-teds'>
+                Furniture for business, made simple.
+              </h2>
+              <p className='buil-divhs'>Say goodbye to cost, complexity, and commitment of furniture. Feather makes it easy with affordable rental plans, white glove delivery and best-in-class account service.</p>
+              <p className='explore-furnitures'>Visit Feather For Business </p>
+            </div>
+          </aside>
+          <aside>
+            <img src={img2} className='asaidinimges' alt="" />
+          </aside>
+        </section>
+        <section className='garge-section'>
+          <aside>
+            <img src={img3} alt="" />
+          </aside>
+          <aside>
+            <h2 className='garage-sale'>Never assemble furniture again.</h2>
+            <p className='garage-saleopen'>We move a lot. There are countless floor plans and roommates (oh, and your evolving tastes) between here and there. With Feather, furnish your space without breaking a sweat or reaching for a screwdriver.</p>
+            <p className='shop-now'>How Feather Works</p>
+          </aside>
         </section>
       </main>
     </div>
